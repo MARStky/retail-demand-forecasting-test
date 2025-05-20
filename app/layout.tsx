ee0bec0b5a9f7e13eca4c5af28c67e9b17122417
Marcella Tang <94746639+MARStky@users.noreply.github.com>
2025-05-20 12:08:51 +0800

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Retail Demand Forecasting',
+  description: 'Retail demand forecasting demo using Amazon SageMaker Autopilot',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
